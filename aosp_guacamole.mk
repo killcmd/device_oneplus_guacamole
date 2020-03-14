b7caca7852ac6cd2f286f7282081d7bcdd70c2ac
Ferdinand Vasquez <killcmd@gmail.com>
2020-03-14 18:20:17 -0400

    Update aosp_guacamole.mk

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
 # Inherit some common Potato stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_guacamole
